142313ba9f2e87933c9b1f0556f72654bd10d2e2
ishaan-codes <ishaangupta.246@gmail.com>
2024-07-29 23:13:30 +0530

    setup project files part 1

---
+++ src/index.js
@@ -0,0 +1,4 @@
+//dev dependencies - these are the dependencies that are only required during development and not in production, thus they don't take up space in the production build
+//dependencies - these are the dependencies that are required in production
+//.gitkeep - this file is used to keep the directory in the git repository even if it is empty
+// nodemon restarts the server automatically and runs index.js when we write npm run dev in the terminal (for this we need to add a script in package.json)
