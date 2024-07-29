8a7bbc0fb691d5d1152daffe7ec7f971674a7af6
ishaan-codes <ishaangupta.246@gmail.com>
2024-07-30 00:00:40 +0530

    add prettier config

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@
 //dependencies - these are the dependencies that are required in production
 //.gitkeep - this file is used to keep the directory in the git repository even if it is empty otherwise git ignores the folders/directories that are empty
 // nodemon restarts the server automatically and runs index.js when we write npm run dev in the terminal (for this we need to add a script in package.json)
+//prettier is a code formatter that formats the code according to the rules that we have set in the .prettierrc file
+//we add two files after installing prettier - .prettierrc and .prettierignore
