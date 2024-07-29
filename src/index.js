348d774af807cb39a3ead09e7b33344cedc75ad0
ishaan-codes <ishaangupta.246@gmail.com>
2024-07-29 23:28:37 +0530

    add structure changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 //dev dependencies - these are the dependencies that are only required during development and not in production, thus they don't take up space in the production build
 //dependencies - these are the dependencies that are required in production
-//.gitkeep - this file is used to keep the directory in the git repository even if it is empty
+//.gitkeep - this file is used to keep the directory in the git repository even if it is empty otherwise git ignores the folders/directories that are empty
 // nodemon restarts the server automatically and runs index.js when we write npm run dev in the terminal (for this we need to add a script in package.json)
